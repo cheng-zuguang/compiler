057141ddcef787c007a935bce1a107d91e25a659
chengzuguang <chengzuguang@qq.com>
2023-10-17 17:59:27 +0800

    add tool/GenerateAST.java

---
+++ Jlox/src/com/craftinginterpreters/tool/GenerateAst.java
@@ -0,0 +1,71 @@
+package com.craftinginterpreters.tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+public class GenerateAst {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: generate_ast <output dir>");
+            System.exit(64);
+        }
+
+        String outputDir = args[0];
+
+        defineAst(outputDir, "Expr", Arrays.asList(
+                "Binary   : Expr left,Token operator,Expr right",
+                "Grouping : Expr expression",
+                "Literal  : Object value",
+                "Unary    : Token operator,Expr right"
+        ));
+    }
+
+    private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
+        String path = outputDir + "/" + baseName + ".java";
+        PrintWriter writer = new PrintWriter(path, "UTF-8");
+
+        writer.println("package com.craftinginterpreters.lox;");
+        writer.println();
+        writer.println("import java.util.List;");
+        writer.println();
+        writer.println("abstract class " + baseName + " {");
+
+        // AST classes
+        for (String type: types) {
+            String className = type.split(":")[0].trim();
+            String fields = type.split(":")[1].trim();
+
+            defineType(writer, baseName, className, fields);
+        }
+
+        writer.println("}");
+        writer.close();
+    }
+
+    private static void defineType(PrintWriter writer, String baseName, String className, String fieldsList) {
+        // define
+        writer.println(" static class " + className + " extends " + baseName + " {");
+        // constructor
+        writer.println("    " + className + "(" + fieldsList + ") {");
+
+        // store params in fields
+        String[] fields = fieldsList.split(",");
+        for (String field : fields) {
+            System.out.println(field);
+            String name = field.split(" ")[1];
+            writer.println("        this." + name + " = " + name + ";");
+        }
+
+        writer.println("    }");
+
+        // fields
+        writer.println();
+        for(String field : fields) {
+            writer.println("    final " + field + ";");
+        }
+
+        writer.println("  }");
+    }
+}
