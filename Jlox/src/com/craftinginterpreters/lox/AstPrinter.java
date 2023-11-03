656767cad6f003cd07c6aca08f70a80290378ec1
chengzuguang <chengzuguang@qq.com>
2023-11-03 10:13:27 +0800

    fix: ASTPrinter.java

--- Jlox/src/com/craftinginterpreters/lox/AstPrinter.java
+++ Jlox/src/com/craftinginterpreters/lox/AstPrinter.java
@@ -1,92 +1,162 @@
-//package com.craftinginterpreters.lox;
-//
-//public class AstPrinter implements Expr.Visitor<String> {
-//    public static void main(String[] args) {
-////        Expr expression = new Expr.Binary(
-////                new Expr.Unary(
-////                        new Token(TokenType.MINUS, "-", null, 1),
-////                        new Expr.Literal(123)
-////                ),
-////                new Token(TokenType.PLUS, "+", null, 1),
-////                new Expr.Grouping(new Expr.Literal(45.67))
-////        );
-//
-//        Expr expression = new Expr.Binary(
-//                new Expr.Grouping(
-//                        new Expr.Binary(
-//                                new Expr.Literal(1),
-//                                new Token(TokenType.PLUS, "+", null, 1),
-//                                new Expr.Literal(2)
-//                        )
-//                ),
-//                new Token(TokenType.STAR, "*", null, 1),
-//                new Expr.Grouping(
-//                        new Expr.Binary(
-//                                new Expr.Literal(4),
-//                                new Token(TokenType.MINUS, "-", null, 1),
-//                                new Expr.Literal(3)
-//                        )
-//                )
-//        );
-//
-//        System.out.println(new AstPrinter().print(expression));
-//    }
-//
-//    String print(Expr expr) {
-//        return expr.accept(this);
-//    }
-//
-//    @Override
-//    public String visitBinaryExpr(Expr.Binary expr) {
-//        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
-//    }
-//
-//    @Override
-//    public String visitGroupingExpr(Expr.Grouping expr) {
-//        return parenthesize("group", expr.expression);
-//    }
-//
-//    @Override
-//    public String visitLiteralExpr(Expr.Literal expr) {
-//        if (expr.value == null) return "nil";
-//        return expr.value.toString();
-//    }
-//
-//    @Override
-//    public String visitUnaryExpr(Expr.Unary expr) {
-//        return parenthesize(expr.operator.lexeme, expr.right);
-//    }
-//
-////    @Override
-////    public String visitConditionalExpr(Expr.Conditional expr) {
-////        return parenthesize("condition", expr.expression);
-////    }
-//
-//    // 实现的效果： (* (- 123) (group 45.67))
-//    private String parenthesize(String name, Expr... exprs) {
-//        StringBuilder builder = new StringBuilder();
-//
-//        builder.append("(").append(name);
-//        for (Expr expr : exprs) {
-//            builder.append(" ");
-//            builder.append(expr.accept(this));
-//        }
-//
-//        builder.append(")");
-//
-//        return builder.toString();
-//    }
-//
-//    // (1 + 2) * (4 - 3) => 1 2 + 4 3 - *
-//    private String parenthesize1(String name, Expr... exprs) {
-//        StringBuilder builder = new StringBuilder();
-//
-//        for (Expr expr : exprs) {
-//            builder.append(expr.accept(this)).append(" ");
-//        }
-//
-//        builder.append(" ").append(name);
-//
-//        return builder.toString();
-//    }
-//}
+package com.craftinginterpreters.lox;
+
+// Creates an unambiguous, if ugly, string representation of AST nodes.
+class AstPrinter implements Expr.Visitor<String>, Stmt.Visitor<String> {
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    String print(Stmt stmt) {
+        return stmt.accept(this);
+    }
+    @Override
+    public String visitBlockStmt(Stmt.Block stmt) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("(block ");
+
+        for (Stmt statement : stmt.statements) {
+            builder.append(statement.accept(this));
+        }
+
+        builder.append(")");
+        return builder.toString();
+    }
+
+    @Override
+    public String visitExpressionStmt(Stmt.Expression stmt) {
+        return parenthesize(";", stmt.expression);
+    }
+
+    @Override
+    public String visitFunctionStmt(Stmt.Function stmt) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("(fun " + stmt.name.lexeme + "(");
+
+        for (Token param : stmt.params) {
+            if (param != stmt.params.get(0)) builder.append(" ");
+            builder.append(param.lexeme);
+        }
+
+        builder.append(") ");
+
+        for (Stmt body : stmt.body) {
+            builder.append(body.accept(this));
+        }
+
+        builder.append(")");
+        return builder.toString();
+    }
+
+    @Override
+    public String visitIfStmt(Stmt.If stmt) {
+        if (stmt.elseBranch == null) {
+            return parenthesize2("if", stmt.condition, stmt.thenBranch);
+        }
+
+        return parenthesize2("if-else", stmt.condition, stmt.thenBranch,
+                stmt.elseBranch);
+    }
+
+    @Override
+    public String visitPrintStmt(Stmt.Print stmt) {
+        return parenthesize("print", stmt.expression);
+    }
+
+    @Override
+    public String visitReturnStmt(Stmt.Return stmt) {
+        if (stmt.value == null) return "(return)";
+        return parenthesize("return", stmt.value);
+    }
+
+    @Override
+    public String visitVarStmt(Stmt.Var stmt) {
+        if (stmt.initialize == null) {
+            return parenthesize2("var", stmt.name);
+        }
+
+        return parenthesize2("var", stmt.name, "=", stmt.initialize);
+    }
+
+    @Override
+    public String visitWhileStmt(Stmt.While stmt) {
+        return parenthesize2("while", stmt.condition, stmt.body);
+    }
+
+    @Override
+    public String visitAssignExpr(Expr.Assign expr) {
+        return parenthesize2("=", expr.name.lexeme, expr.value);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+    }
+
+    @Override
+    public String visitCallExpr(Expr.Call expr) {
+        return parenthesize2("call", expr.callee, expr.arguments);
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return parenthesize("group", expr.expression);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null) return "nil";
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitLogicalExpr(Expr.Logical expr) {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return parenthesize(expr.operator.lexeme, expr.right);
+    }
+
+    @Override
+    public String visitVariableExpr(Expr.Variable expr) {
+        return expr.name.lexeme;
+    }
+    private String parenthesize(String name, Expr... exprs) {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("(").append(name);
+        for (Expr expr : exprs) {
+            builder.append(" ");
+            builder.append(expr.accept(this));
+        }
+        builder.append(")");
+
+        return builder.toString();
+    }
+    // Note: AstPrinting other types of syntax trees is not shown in the
+    // book, but this is provided here as a reference for those reading
+    // the full code.
+    private String parenthesize2(String name, Object... parts) {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("(").append(name);
+
+        for (Object part : parts) {
+            builder.append(" ");
+
+            if (part instanceof Expr) {
+                builder.append(((Expr)part).accept(this));
+            } else if (part instanceof Stmt) {
+                builder.append(((Stmt) part).accept(this));
+            } else if (part instanceof Token) {
+                builder.append(((Token) part).lexeme);
+            } else {
+                builder.append(part);
+            }
+        }
+        builder.append(")");
+
+        return builder.toString();
+    }
+}
