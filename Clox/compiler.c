64b9f548c894aa676f9ed30945f7c6ca622a3efd
chengzuguang <chengzuguang@qq.com>
2023-11-29 22:58:52 +0800

    support control flow

--- Clox/compiler.c
+++ Clox/compiler.c
@@ -77,10 +77,9 @@ typedef struct {
 } Compiler;
 
 static ParseRule *getRule(TokenType type);
-
 static void parsePrecedence();
-
 static void expression();
+static void and_(bool canAssign);
 
 static uint8_t identifierConstant(Token* name);
 static int resolveLocal(Compiler* compiler, Token* name);
@@ -169,7 +168,18 @@ static void emitBytes(uint8_t byte1, uint8_t byte2) {
     emitByte(byte2);
 }
 
-// back-patching(回溯)
+static void emitLoop(int loopStart) {
+    emitByte(OP_LOOP);
+
+    // the +2 is to take account the size of the op_loop instruction's own operand which we also need to jump over.
+    int offset = currentChunk()->count - loopStart + 2;
+    if (offset > UINT16_MAX) error("Loop body too large.");
+
+    emitByte((offset >> 8) & 0xff);
+    emitByte(offset & 0xff);
+}
+
+// back-patching(回填)
 // emit the jump instruction first with placeholder offset operand.
 // then we compile the then body, we know how far to jump.
 // so we go back and replace that placeholder offset operand.
@@ -206,12 +216,15 @@ static void emitConstant(Value value) {
 
 static void patchJump(int offset) {
     // -2 to adjust for the bytecode for the jump offset itself.
+    // current - (beforeCurrent -2) - 2 = current - beforeCurrent
+    // equal the distance of jump offset to code block.
     int jump = currentChunk()->count - offset - 2;
 
     if (jump > UINT16_MAX) {
         error("Too much code to jump.");
     }
 
+    // store the distance of the jump separately
     currentChunk()->code[offset] = (jump >> 8) & 0xff;
     currentChunk()->code[offset + 1] = jump & 0xff;
 }
@@ -262,6 +275,19 @@ static void number(bool canAssign) {
     // 将字符串中的number literals 转为 double.
     double value = strtod(parser.previous.start, NULL);
     emitConstant(NUMBER_VAL(value));
+}
+
+static void or_(bool canAssign) {
+    int elseJump = emitJump(OP_JUMP_IF_FALSE);
+    int endJump = emitJump(OP_JUMP);
+
+    // when the left operand if falsey, then we unconditional jump over the code for the right operand.
+    // otherwise skip the right operand.
+    patchJump(elseJump);
+    emitByte(OP_POP);
+
+    parsePrecedence(PREC_OR);
+    patchJump(endJump);
 }
 
 // case: str-variable.
@@ -415,7 +441,7 @@ ParseRule rules[] = {
         [TOKEN_IDENTIFIER]    = {variable, NULL, PREC_NONE},
         [TOKEN_STRING]        = {string, NULL, PREC_NONE},
         [TOKEN_NUMBER]        = {number, NULL, PREC_NONE},
-        [TOKEN_AND]           = {NULL, NULL, PREC_NONE},
+        [TOKEN_AND]           = {NULL, and_, PREC_AND},
         [TOKEN_CLASS]         = {NULL, NULL, PREC_NONE},
         [TOKEN_ELSE]          = {NULL, NULL, PREC_NONE},
         [TOKEN_FALSE]         = {literal, NULL, PREC_NONE},
@@ -423,7 +449,7 @@ ParseRule rules[] = {
         [TOKEN_FUN]           = {NULL, NULL, PREC_NONE},
         [TOKEN_IF]            = {NULL, NULL, PREC_NONE},
         [TOKEN_NIL]           = {literal, NULL, PREC_NONE},
-        [TOKEN_OR]            = {NULL, NULL, PREC_NONE},
+        [TOKEN_OR]            = {NULL, or_, PREC_OR},
         [TOKEN_PRINT]         = {NULL, NULL, PREC_NONE},
         [TOKEN_RETURN]        = {NULL, NULL, PREC_NONE},
         [TOKEN_SUPER]         = {NULL, NULL, PREC_NONE},
@@ -568,6 +594,16 @@ static void defineVariable(uint8_t global) {
     emitBytes(OP_DEFINE_GLOBAL, global);
 }
 
+static void and_(bool canAssign) {
+    int endJump = emitJump(OP_JUMP_IF_FALSE);
+
+    emitByte(OP_POP);
+    parsePrecedence(PREC_AND);
+
+    // if the left value is false, then will jump right operand.
+    patchJump(endJump);
+}
+
 static ParseRule *getRule(TokenType type) {
     return &rules[type];
 }
@@ -607,11 +643,60 @@ static void expressionStatement() {
     // evaluates the expression and discard the result. example: eat("apple");
     emitByte(OP_POP);
 }
+// forStmt    →   "for" "("  ( varDecl | exprStmt | ";" ) expression? ";" expression? ")" statement ;
+static void forStatement() {
+    beginScope();
+    consume(TOKEN_LEFT_PAREN, "Excepted '(' after the for.");
+
+    // initializer
+    if (match(TOKEN_SEMICOLON)) {
+        // no initializer
+    } else if (match(TOKEN_VAR)) {
+        varDeclaration();
+    } else {
+        expressionStatement();
+    }
+
+    int loopStart = currentChunk()->count;
+    // exit the loop
+    int exitJump = -1;
+    if (!match(TOKEN_SEMICOLON)) {
+        expression();
+        consume(TOKEN_SEMICOLON, "Expect ';' after loop condition.");
+
+        // jump out of the loop if the condition is false.
+        exitJump = emitJump(OP_JUMP_IF_FALSE);
+        emitByte(OP_POP);
+    }
+
+    if (!match(TOKEN_RIGHT_PAREN)) {
+        int bodyJump = emitJump(OP_JUMP);
+        int incrementStart = currentChunk()->count;
+        expression();
+        emitByte(OP_POP);
+        consume(TOKEN_RIGHT_PAREN, "Excepted ')' after clauses.");
+
+        // jump over the increment, run the body, and jump back up the increment, run it.
+        emitLoop(loopStart);
+        loopStart = incrementStart;
+        patchJump(loopStart);
+    }
+
+    statement();
+    emitLoop(loopStart);
+
+    if (exitJump != -1) {
+        patchJump(exitJump);
+        emitByte(OP_POP);
+    }
+
+    endScope();
+}
 
 // ifStmt         → "if" "(" expression ")" statement
 //                  ( "else" statement )? ;
 static void ifStatement() {
-    consume(TOKEN_LEFT_PAREN, "Excepted '(' after the if statement.");
+    consume(TOKEN_LEFT_PAREN, "Excepted '(' after the if.");
     expression();
     consume(TOKEN_RIGHT_PAREN, "Excepted ')' after the condition.");
 
@@ -635,6 +720,23 @@ static void printStatement() {
     expression();
     consume(TOKEN_SEMICOLON, "Expect ';' after value.");
     emitByte(OP_PRINT);
+}
+
+// whileStmt      → "while" "(" expression ")" statement ;
+static void whileStatement() {
+    int loopStart = currentChunk()->count;
+    consume(TOKEN_LEFT_PAREN, "Excepted '(' after the while.");
+    expression();
+    consume(TOKEN_RIGHT_PAREN, "Excepted ')' after the condition.");
+
+    int exitJump = emitJump(OP_JUMP_IF_FALSE);
+    emitByte(OP_POP);
+    statement();
+    // jumping forward.
+    emitLoop(loopStart);
+
+    patchJump(exitJump);
+    emitByte(OP_POP);
 }
 
 /**
@@ -682,6 +784,10 @@ static void statement() {
         printStatement();
     } else if (match(TOKEN_IF)) {
         ifStatement();
+    } else if (match(TOKEN_WHILE)) {
+        whileStatement();
+    } else if (match(TOKEN_FOR)) {
+        forStatement();
     } else if (match(TOKEN_LEFT_BRACE)) {
         beginScope();
         block();
